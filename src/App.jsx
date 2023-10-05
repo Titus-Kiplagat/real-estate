664c5bbe3bb8edf968d27422f6ccdafb8f13ce61
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-05 23:25:07 +0300

    Import relevant components to display markup structure on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import About from "./components/About"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import Features from './components/Features';
+import OnSale from "./components/OnSale";
+import Service from "./components/Service";
+import View from "./components/View";
+import Clients from "./components/Clients";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <h1 className="text-red-800">Vite + React</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Features />
+      <OnSale />
+      <Service />
+      <View />
+      <Clients />
+      <Contact />
+      <Footer />
     </>
   )
 }
